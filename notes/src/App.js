434e871729a0a2df8e8beafa1e3cb7ff1a0921ff
Abhijith-Reddy-ch <abhijithreddychalamalla@gmail.com>
2025-05-16 18:07:14 +0530

    notes ui

---
+++ notes/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import uuid from "react-uuid";
+import './App.css';
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+
+ 
+function App() {
+ const[notes, setNotes]= useState([]);
+const[activeNote, setActiveNote] = useState(false);
+ const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note)=>note.id !== idToDelete))
+  };
+
+
+
+  return (
+    <div className="App"> 
+      <Sidebar notes={notes} 
+      onAddNote={onAddNote}
+       onDeleteNote={onDeleteNote}
+       activeNote = {activeNote}
+       setActiveNote = {setActiveNote}/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
